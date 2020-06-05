5e8bb959d19a9a41160bf3eebe8ccfb6b1576c2d
Pamylla <pamyllaostermann@gmail.com>
2020-06-05 19:54:28 -0300

    #Create function to get UF - by IBGE

---
+++ scripts/create-point.js
@@ -0,0 +1,26 @@
+document
+    .querySelector("select[uf]")
+     .addEventListener("change", () => {
+        console.log('mudei')
+     } ) 
+
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(res => res.json())
+        .then(states => {
+
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
+}
+
+// populateUFs()
+
+// function getCities() {
+//     const citySelect = document.querySelector("select[name=city]")
+//     console.log(event.target)
+// }

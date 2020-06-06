9804960c3eda66552ab340751346e118fafa9b49
Pamylla <pamyllaostermann@gmail.com>
2020-06-06 15:02:42 -0300

    # Interative add class to element html or remove, using click

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -44,3 +44,26 @@ function getCities() {
     .addEventListener("change", getCities)
 
 
+//itens de coleta
+//pegar todos os li
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (let item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+// .dataset.id = vai pegar os numeros que foram colocados no id no html - data-id="1" 
+// function handleSelectedItem(event){
+//     console.log(event.target.dataset.id)
+// }
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    //add ou remover uma classe no js - ele olha o elemento li, exite 'selecte: n , então vou add, se tem, tira. 
+    itemLi.classList.toggle("selected")
+
+    const itemId= itemLi.dataset.id 
+
+}

0cc55608508f6926c42a018375543bb61b3dbe75
Pamylla <pamyllaostermann@gmail.com>
2020-06-25 10:32:42 -0300

    #Add template engine - nunjucks

--- src/server.js
+++ src/server.js
@@ -4,14 +4,22 @@ const server = express()
 //3ºconfigurar pasta publica
 server.use(express.static("public"))
 
+//4ºutilizando template engine - mundanças no html tipo ngfor*
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 //Configurar rotas
 //2º pag inicial
 server.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+    return res.render("index.html");
 });
 
 server.get("/create-point", function (req, res) {
-    res.sendFile(__dirname + "/views/create-point.html");
+    return res.render("create-point.html");
 });
 
 //1º ligar o servidor
